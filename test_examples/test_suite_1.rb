a2a0162dcd1ecfbb9b0dde86fdc4f8aba161b7ea
Tina Yan <yant1@mozy.com>
2016-04-28 12:36:08 +0800

    update comments for tset suite 1 and 2

--- test_examples/test_suite_1.rb
+++ test_examples/test_suite_1.rb
@@ -4,7 +4,7 @@
 ##            <testset> could be smoke/regression/integration. It defines different cases array for different test set
 ################################################################################
 
-require "./common_lib.rb"
+require "./common_lib.rb"  ##This is common libraries you want to share among test suites.
 
 class SuiteOneClass
   include CommonLib
@@ -20,13 +20,18 @@ class SuiteOneClass
   puts "This is test suite one!"
   end
 
+  ## This pre setup method for suite. It will be executed before the whole suite is executed
   def pre_setup_suite()
     @cfg['pre_setup'] ="defined"
     return true
   end
+
+  ## This tear down method for suite. It will be executed after the whole suite is executed
   def tear_down_suite()
     return "deleted!"
   end
+
+  ## This is somke test set definition. This smoke test will only run case_001 and case_002
   def smoke_test ()
     smoke_test = [
         "case_001",
@@ -34,9 +39,14 @@ class SuiteOneClass
     ]
     return smoke_test
   end
+
+  ## This is integration test set definition. This integration test will run all test cases.
+  ## By default, regression test will also run all test cases. So you don't need to define regression test set
   def integration_test()
     return ["all"]
   end
+
+  ## your test cases:
   def case_001
     puts "case 001"
     ret_res= {"test_value"=>"001",
@@ -57,6 +67,7 @@ class SuiteOneClass
     return func_diff(5,1)
   end
 
+## private methods are only for internal calls
 private
   def func_sum (a, b)
      return a+b
--- test_examples/test_suite_2.rb
+++ test_examples/test_suite_2.rb
@@ -16,19 +16,30 @@ class SuiteTwoClass
     puts "This is test suite two!"
 
   end
+
+  ## This pre setup method for case. It will be executed before each case is executed
   def pre_setup_case()
     return "pre_setup_for_case"
   end
+
+  ## This tear down method for case. It will be executed after  each case is executed
   def tear_down_case()
     return "tear_down_for_case"
   end
+
+  ## This is somke test set definition. This smoke test will only run case_aaaa
   def smoke_test()
     return ["case_aaaa"]
   end
+
+  ## This is integration test set definition. This Integration test will run case_aaaa and case_cccc.
+  ## By default, regression test will also run all test cases. So you don't need to define regression test set
   def integration_test()
     int =["case_aaaa","case_cccc"]
     return int
   end
+
+  ## The followings are your test cases
   def case_aaaa()
     puts "case aaaa"
     return 003
